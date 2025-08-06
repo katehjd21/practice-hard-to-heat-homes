bb919f97b56a8e999de67c2c3ca5fbc3dba27676
katehjd21 <katedrew2103@hotmail.co.uk>
2025-08-06 17:43:31 +0100

    initial commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "kd-test-terraform-bucket"
+    key            = "practice_hard_to_heat_homes/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
